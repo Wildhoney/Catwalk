62f78e7bb124297f30c07c5c76043d645ce97d02
Adam Timberlake <adam.timberlake@gmail.com>
2013-11-19 00:27:24 +0000

    Updated package file for rename

--- example/default.js
+++ example/default.js
@@ -1,12 +1,5 @@
 (function($catwalk) {
 
-    var model = new $catwalk.Model.Cat({
-        id      : 1,
-        age     : 28,
-        name    : 'Adam',
-        colours : [1,2,3]
-    });
-
     $catwalk.Model.Cat.on('create', function onCreate(promise, primaryKey) {
         promise.resolve();
     });
@@ -22,5 +15,14 @@
     $catwalk.Model.Cat.on('delete', function onDelete(promise, model) {
         promise.resolve();
     });
+
+    var model = new $catwalk.Model.Cat({
+        id      : 1,
+        age     : 28,
+        name    : 'Adam',
+        colours : [1,2,3]
+    });
+
+    model.save();
 
 })(window.catwalk);
