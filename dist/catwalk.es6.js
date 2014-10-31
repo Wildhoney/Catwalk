0f628e23c543d58f2ea69892479a2feb66c46889
Wildhoney <adam.timberlake@gmail.com>
2014-10-31 21:27:56 +0000

    Built ES5 and ES6 versions

---
+++ dist/catwalk.es6.js
@@ -0,0 +1,28 @@
+//(function($Catwalk) {
+//
+//    "use strict";
+//
+//    /**
+//     * @class Catwalk
+//     * @author Adam Timberlake
+//     * @link https://github.com/Wildhoney/Catwalk.js
+//     */
+//    class CatwalkCollection {
+//
+//        /**
+//         * @method constructor
+//         * @param {String} name
+//         * @param {Object} properties
+//         * @return {CatwalkCollection}
+//         */
+//        constructor(name, properties) {
+//            this._name       = name;
+//            this._properties = properties;
+//        }
+//
+//    }
+//
+//    // Expose the `Catwalk.Collection` property.
+//    $Catwalk.Collection = CatwalkCollection;
+//
+//})(window.Catwalk);
