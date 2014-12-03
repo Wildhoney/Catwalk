7d2d2baf8676048eca486668e03e6a395bfc6507
Wildhoney <adam.timberlake@gmail.com>
2014-12-03 01:34:03 +0000

    Added beer API. Relates to #24

--- example/js/Default.js
+++ example/js/Default.js
@@ -1,30 +1,28 @@
 (function main($angular, $catwalk) {
 
     // Everything for a reason...
-    $angular.module('catwalkApp', []).controller('CatsController', function CatsController($scope) {
+    $angular.module('catwalkApp', []).controller('BeersController', function BeersController($scope, $http) {
 
         /**
          * @property collection
          * @type {Collection}
          */
-        $scope.collection = $catwalk.createCollection('cats', {
-            name: $catwalk.typecast.string()
+        $scope.collection = $catwalk.createCollection('beers', {
+            name:        $catwalk.typecast.string(),
+            description: $catwalk.typecast.string(),
+            abv:         $catwalk.typecast.number()
         });
-
-        // Add some initial cat models.
-        $scope.collection.createModel({ name: 'Kipper' });
-        $scope.collection.createModel({ name: 'Splodge' });
-        $scope.collection.createModel({ name: 'Mango' });
-        $scope.collection.createModel({ name: 'Busters' });
-        $scope.collection.createModel({ name: 'Miss Kittens' });
-        $scope.collection.createModel({ name: 'Tinker' });
-        $scope.collection.createModel({ name: 'Merlin' });
 
         // Fetch all of the models from the collection.
         $scope.models = $scope.collection.extensibleIteration();
 
         $catwalk.on('refresh', function onRefresh() {
             $scope.models = $scope.collection.extensibleIteration();
+        });
+
+        // Fetch a list of all the beers.
+        $http.jsonp('http://api.openbeerdatabase.com/v1/beers.json?callback=JSON_CALLBACK').then(function then(response) {
+            $scope.collection.addModels(response.data.beers);
         });
 
     });
