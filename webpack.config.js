88d513d8740afde1b6f263821207a95e4e3714f8
Wildhoney <adam.timberlake@gmail.com>
2015-11-18 16:21:07 +0000

    Added build for other files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        collection: './src/collection.js'
+        collection: './src/collection.js',
+        event: './src/event.js',
+        typecast: './src/typecast.js'
     },
     output: {
         path: path.join(__dirname, 'build'),
