752767e7211af980f8cd97298e5263f2b5d847d3
Wildhoney <adam.timberlake@gmail.com>
2015-11-18 14:34:16 +0000

    Added CRUD methods, and WeakMap

--- src/collection.js
+++ src/collection.js
@@ -1,6 +1,12 @@
 import {parse} from './collection/parse';
 
 /**
+ * @constant WeakMap
+ * @type {WeakMap}
+ */
+const collections = new WeakMap();
+
+/**
  * @module Catwalk
  * @submodule Collection
  * @author Adam Timberlake
@@ -15,8 +21,44 @@ class Collection {
      * @return {Collection}
      */
     constructor(name, properties) {
-        this.name = name;
-        this.properties = parse(properties);
+        collections[this] = { name, properties: parse(properties) };
+    }
+
+    /**
+     * @method create
+     * @param {Object} properties
+     * @return {Promise}
+     */
+    create(properties) {
+
+    }
+
+    /**
+     * @method update
+     * @param {Object} model
+     * @param {Object} properties
+     * @return {Promise}
+     */
+    update(model, properties) {
+
+    }
+
+    /**
+     * @method read
+     * @param {Object} properties
+     * @return {Promise}
+     */
+    read(properties) {
+
+    }
+
+    /**
+     * @method delete
+     * @param {Object} model
+     * @return {Promise}
+     */
+    delete(model) {
+
     }
 
 }
