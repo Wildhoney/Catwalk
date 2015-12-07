109a27a72dbc1f806ae035dcce94340dbce61ce2
Wildhoney <adam.timberlake@gmail.com>
2015-12-07 10:13:05 +0000

    Changed import to export

--- src/default.js
+++ src/default.js
@@ -1,3 +1,3 @@
 export {collection} from './collection.js';
-import {field, cast, compose} from './field.js';
-import {on, type} from './event.js';
+export {field, cast, compose} from './field.js';
+export {on, type} from './event.js';
